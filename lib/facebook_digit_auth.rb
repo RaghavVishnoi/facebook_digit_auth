dbdcaaebcaa8247f5de905b5963b8d14734114e8
raghavpratap <raghav.pratap@trantorinc.com>
2017-07-26 13:05:39 +0530

    Added google_auth_client file with require

--- lib/facebook_digit_auth.rb
+++ lib/facebook_digit_auth.rb
@@ -1,6 +1,7 @@
 require "facebook_digit_auth/version"
 require "facebook_client"
 require "digit_client"
+require "google_auth_client"
 
 module FacebookDigitAuth 
  	class BestAuth
@@ -10,7 +11,6 @@ module FacebookDigitAuth
 			elsif type == 'digit'
 				DigitsClient.new(params).data
 			elsif type == 'google'
-
 				GoogleAuthClient.new(params).data			
 			end
 		end
